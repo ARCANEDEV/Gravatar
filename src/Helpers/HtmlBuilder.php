354f5e8d0eef14eda7fb9eb862c408027b837c9f
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-03 23:27:09 +0100

    Adding HtmlBuilder Helper

---
+++ src/Helpers/HtmlBuilder.php
@@ -0,0 +1,65 @@
+<?php namespace Arcanedev\Gravatar\Helpers;
+
+class HtmlBuilder
+{
+    /**
+     * Generate an HTML image element.
+     *
+     * @param  string  $url
+     * @param  string  $alt
+     * @param  array   $attributes
+     *
+     * @return string
+     */
+    public static function image($url, $alt = null, $attributes = [])
+    {
+        $attributes['alt'] = $alt;
+
+        return '<img src="' . $url . '"' . self::attributes($attributes) . '>';
+    }
+
+
+    /**
+     * Build an HTML attribute string from an array.
+     *
+     * @param  array  $attributes
+     *
+     * @return string
+     */
+    public static function attributes($attributes)
+    {
+        $html = [];
+
+        // For numeric keys we will assume that the key and the value are the same
+        // as this will convert HTML attributes such as "required" to a correct
+        // form like required="required" instead of using incorrect numerics.
+        foreach ((array) $attributes as $key => $value) {
+            $html[] = static::attributeElement($key, $value);
+        }
+
+        $html = array_filter($html);
+
+        return count($html) > 0 ? ' ' . implode(' ', $html) : '';
+    }
+
+    /**
+     * Build a single attribute element.
+     *
+     * @param  string  $key
+     * @param  string  $value
+     *
+     * @return string|null
+     */
+    private static function attributeElement($key, $value)
+    {
+        if (is_numeric($key)) {
+            $key = $value;
+        }
+
+        if ( ! is_null($value)) {
+            return $key . '="' . htmlentities($value, ENT_QUOTES, 'UTF-8', false) . '"';
+        }
+
+        return null;
+    }
+}
---
+++ tests/Helpers/HtmlBuilderTest.php
@@ -0,0 +1,47 @@
+<?php namespace Arcanedev\Gravatar\Tests\Helpers;
+
+use Arcanedev\Gravatar\Helpers\HtmlBuilder;
+use Arcanedev\Gravatar\Tests\TestCase;
+
+/**
+ * Class     HtmlBuilderTest
+ *
+ * @package  Arcanedev\Gravatar\Tests\Helpers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class HtmlBuilderTest extends TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Test Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @test */
+    public function it_can_build_image_tag()
+    {
+        $this->assertEquals(
+            '<img src="assets/img/logo.png" class="img-responsive" required="required" alt="Logo">',
+            HtmlBuilder::image('assets/img/logo.png', 'Logo', [
+                'class'  => 'img-responsive', 'required'
+            ])
+        );
+    }
+}
