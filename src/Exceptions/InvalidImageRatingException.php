7c99963e78a81342adf362af888bf361e093ccbc
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-03 20:23:09 +0100

    Adding more Exceptions

---
+++ src/Exceptions/InvalidImageRatingException.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Gravatar\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * Class     InvalidImageRatingException
+ *
+ * @package  Arcanedev\Gravatar\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InvalidImageRatingException extends InvalidArgumentException {}
---
+++ src/Exceptions/InvalidImageUrlException.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Gravatar\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * Class     InvalidImageUrlException
+ *
+ * @package  Arcanedev\Gravatar\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InvalidImageUrlException extends InvalidArgumentException {}
