e22a25be8295b2cec3a6eab028de669cabf07403
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-03 19:09:49 +0100

    Adding InvalidImageSizeException

---
+++ src/Exceptions/InvalidImageSizeException.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Gravatar\Exceptions;
+
+use InvalidArgumentException;
+
+/**
+ * Class     InvalidImageSizeException
+ *
+ * @package  Arcanedev\Gravatar\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InvalidImageSizeException extends InvalidArgumentException {}
