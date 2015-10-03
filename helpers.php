f4506e8dd718c2e90f5e1597f78ac90a90ff86db
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-03 19:09:26 +0100

    Adding the gravatar helper

--- helpers.php
+++ helpers.php
@@ -1,2 +1,12 @@
 <?php
-//
+
+if ( ! function_exists('gravatar')) {
+    /**
+     * Get the gravatar instance.
+     *
+     * @return \Arcanedev\Gravatar\Gravatar
+     */
+    function gravatar() {
+        return app('arcanedev.gravatar');
+    }
+}
