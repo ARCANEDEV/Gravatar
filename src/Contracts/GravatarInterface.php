65ee4f8eb9ece408435b0d7cd20dbfd9ab686791
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-03 20:24:11 +0100

    Adding the Gravatar Interface

---
+++ src/Contracts/GravatarInterface.php
@@ -0,0 +1,16 @@
+<?php namespace Arcanedev\Gravatar\Contracts;
+
+/**
+ * Interface  GravatarInterface
+ *
+ * @package   Arcanedev\Gravatar\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface GravatarInterface
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+
+}
